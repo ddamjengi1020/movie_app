7f0d271b1f348f9d3ebf1d30aca742e5eda4c313
ddamjengi <silluat11@gmail.com>
2020-02-13 01:24:19 +0900

    jsx & props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import propTypes from "prop-types";
 
-function App() {
+const Food = ({ name, rating }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I like {name}</h2>
+      <h3>rating: {rating}</h3>
     </div>
   );
-}
+};
+
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  rating: propTypes.number
+};
+
+const foodList = [
+  {
+    id: 1,
+    name: "test1"
+  },
+  {
+    id: 2,
+    name: "test2",
+    rating: 4
+  },
+  {
+    id: 3,
+    name: "test3",
+    rating: 3
+  }
+];
+
+const App = () => {
+  return (
+    <div>
+      {foodList.map(el => (
+        <Food key={el.id} name={el.name} rating={el.rating} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
