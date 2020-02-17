029d3afbba428f6be044f5dff73ad6a664d9bec1
ddamjengi <silluat11@gmail.com>
2020-02-18 02:17:57 +0900

    yts api data get + movie component create

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
 import React from "react";
-import propTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-const Food = ({ name, rating }) => {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h3>rating: {rating}</h3>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  rating: propTypes.number
-};
-
-const foodList = [
-  {
-    id: 1,
-    name: "test1"
-  },
-  {
-    id: 2,
-    name: "test2",
-    rating: 4
-  },
-  {
-    id: 3,
-    name: "test3",
-    rating: 3
+  getMovie = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovie();
   }
-];
-
-const App = () => {
-  return (
-    <div>
-      {foodList.map(el => (
-        <Food key={el.id} name={el.name} rating={el.rating} />
-      ))}
-    </div>
-  );
-};
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loading">
+            <span className="loading__text">loading...</span>
+          </div>
+        ) : (
+          movies.map(movie => (
+            <Movie
+              key={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          ))
+        )}
+      </section>
+    );
+  }
+}
 
 export default App;
